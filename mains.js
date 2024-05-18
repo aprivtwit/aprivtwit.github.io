bc009075d0aa27ac52ffbf976dbd4c74e40fc29d
lol123 <none>
2024-05-18 21:53:20 +0200

    lil buddy glazing

--- mains.js
+++ mains.js
@@ -1,33 +1,12 @@
-// Get the current page URL
-const currentPage = window.location.pathname;
-const pageNames = {
-  '/': 'home',
-  '/projects/projects.html': 'projects',
-  '/about/about.html': 'about',
-  '/posts/blog1.html': 'welcome'
-};
+document.addEventListener("DOMContentLoaded", function() {
+  const joinWaitlistButton = document.getElementById("join-waitlist-button");
+  const waitlistFormContainer = document.getElementById("waitlist-form-container");
 
-// Function to generate breadcrumbs
-function generateBreadcrumbs() {
-  const breadcrumbContainer = document.querySelector('.breadcrumbs');
-  const pathSegments = currentPage.split('/').filter(
-      segment => segment !== '');  // Split the URL and remove empty segments
+  console.log("DOM fully loaded and parsed");
 
-  let breadcrumbPath = '<a href="/">home</a>';
-
-  for (let i = 1; i < pathSegments.length; i++) {
-    const path = `/${pathSegments.slice(0, i + 1).join('/')}`;
-    let pageName = pageNames[path] || pathSegments[i];
-
-    if (path === '/posts/blog1.html') {
-      breadcrumbPath += ` / <a href="/posts/post.html">posts</a> / ${pageName}`;
-    } else {
-      breadcrumbPath += ` / <a href="${path}">${pageName}</a>`;
-    }
-  }
-
-  breadcrumbContainer.innerHTML = breadcrumbPath;
-}
-
-// Call the function to generate breadcrumbs1
-generateBreadcrumbs();
+  joinWaitlistButton.addEventListener("click", function(event) {
+    event.preventDefault(); // Prevent the default anchor behavior
+    console.log("Join waitlist button clicked");
+    waitlistFormContainer.style.display = "block"; // Show the form
+  });
+});
